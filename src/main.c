d8ef85a5141025c6530b0227cb223e49a13a0072
jimao <jimao@jimaodeMacBook-Pro.local>
2022-04-01 12:40:38 +0800

    feat: 十亿级别

--- src/main.c
+++ src/main.c
@@ -2,6 +2,14 @@
 #include <stdio.h>
 
 int main() {
-  printf("你好，世界\n");
+  int num = 0;
+  for (int n1 = 0; n1 < 1000; ++n1) {
+    for (int n1 = 0; n1 < 1000; ++n1) {
+      for (int n1 = 0; n1 < 1000; ++n1) {
+        ++num;
+      }
+    }
+  }
+  printf("你好，世界 %d\n", num);
   return 0;
 }
